82ad8f772c3db5be91dffd897d94deb688f1e748
unsualc@likelion.org <unusualc@likelion.org>
2023-06-20 00:36:01 +0900

    pophory server begins

---
+++ src/main/java/com/pophory/pophoryserver/PophoryserverApplication.java
@@ -0,0 +1,13 @@
+package com.pophory.pophoryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PophoryserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PophoryserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pophory/pophoryserver/PophoryserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pophory.pophoryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PophoryserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
