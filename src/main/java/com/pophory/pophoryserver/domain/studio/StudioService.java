0957524ee0ea3c951b27eb09f2706d22f621c63b
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-06-28 16:55:38 +0900

    [feat #8] StudioService 작성

--- src/main/java/com/pophory/pophoryserver/domain/studio/StudioService.java
+++ src/main/java/com/pophory/pophoryserver/domain/studio/StudioService.java
@@ -1,2 +1,20 @@
-package com.pophory.pophoryserver.domain.studio;public class StudioService {
+package com.pophory.pophoryserver.domain.studio;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class StudioService {
+    private final StudioJpaRepository studioJpaRepository;
+
+    @Transactional
+    public StudioResponseDTO findAll() {
+        List<Studio> studios = studioJpaRepository.findAll();
+
+        return StudioResponseDTO.of(studios);
+    }
 }
