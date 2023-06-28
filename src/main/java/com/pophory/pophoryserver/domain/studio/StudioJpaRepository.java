bc8943bcce59f71b50fdf810b2f63f61a8f8afdb
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-06-28 16:54:16 +0900

    [feat #8] StudioJpaRepository 추가

--- src/main/java/com/pophory/pophoryserver/domain/studio/StudioJpaRepository.java
+++ src/main/java/com/pophory/pophoryserver/domain/studio/StudioJpaRepository.java
@@ -1,2 +1,9 @@
-package com.pophory.pophoryserver.domain.studio;public class StudioJpaRepository {
+package com.pophory.pophoryserver.domain.studio;
+
+import com.pophory.pophoryserver.domain.member.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface StudioJpaRepository extends JpaRepository<Studio, Long> {
 }
