3db51dc1087d5213f3013fcd5e1a7940e6155147
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-06-28 17:00:18 +0900

    [feat #8] 사진관 목록 조회 API 구현

--- src/main/java/com/pophory/pophoryserver/domain/studio/StudioController.java
+++ src/main/java/com/pophory/pophoryserver/domain/studio/StudioController.java
@@ -1,2 +1,22 @@
-package com.pophory.pophoryserver.domain.studio;public class StudioController {
+package com.pophory.pophoryserver.domain.studio;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/studios")
+public class StudioController {
+
+    private final StudioService studioService;
+
+    @GetMapping
+    public ResponseEntity<StudioResponseDTO> getAllStudio() {
+        StudioResponseDTO allStudio = studioService.findAll();
+
+        return ResponseEntity.ok(allStudio);
+    }
 }
