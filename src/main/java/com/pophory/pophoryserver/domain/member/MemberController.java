7b177755e6cde2d89bbd447e7754e695551cbcf0
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-06-29 22:34:21 +0900

    [feat #10] MemberController 구현

--- src/main/java/com/pophory/pophoryserver/domain/member/MemberController.java
+++ src/main/java/com/pophory/pophoryserver/domain/member/MemberController.java
@@ -1,4 +1,21 @@
 package com.pophory.pophoryserver.domain.member;
 
+import com.pophory.pophoryserver.domain.member.dto.request.MemberCreateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/member")
 public class MemberController {
+    public final MemberService memberService;
+
+    @PatchMapping
+    public ResponseEntity.HeadersBuilder patchMember(MemberCreateRequestDto memberCreateRequestDto) {
+        memberService.create(memberCreateRequestDto);
+        return ResponseEntity.noContent();
+    }
 }
