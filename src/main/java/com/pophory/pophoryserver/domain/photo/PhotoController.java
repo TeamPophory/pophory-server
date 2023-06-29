51d6ad30f690ca7e9d8eaba462233fa775c4bc63
unanchoi <unusualc@likelion.org>
2023-06-30 01:37:34 +0900

    [feat #7] 사진 추가 API 구현

---
+++ src/main/java/com/pophory/pophoryserver/domain/photo/PhotoController.java
@@ -0,0 +1,24 @@
+package com.pophory.pophoryserver.domain.photo;
+
+import com.pophory.pophoryserver.domain.photo.dto.PhotoAddRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/v1/photo")
+@RequiredArgsConstructor
+public class PhotoController {
+
+    private final PhotoService photoService;
+
+    @PostMapping
+    public ResponseEntity<Void> addPhoto(@RequestPart MultipartFile photo, @Valid PhotoAddRequestDto request, @RequestHeader Long memberId) {
+        photoService.addPhoto(photo, request, memberId);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
