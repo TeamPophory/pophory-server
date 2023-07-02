5c0848f7e53de72cf88829196b28a8f0a2e85168
Yunseo Kang <65678579+yungu0010@users.noreply.github.com>
2023-07-02 10:17:09 +0900

    [feat #23] 마이페이지 조회 API 구현 (#27)
    * [fix #23] conflict 해결
    
    * [fix #23] @AllArgsConstructor private으로 변경
    
    * [feat #23] MemberService 작성
    - 모든 앨범의 사진 개수 가져오는 getPhotoCount 작성
    - 모든 앨범의 사진 정보 가져오는 PhotoListGetResponseDto 작성
    - 마이페이지 조회에 필요한 getMypageMember 작성
    
    * [feat #23] MemberController에 마이페이지 조회 코드 작성
    
    * [fix #23] 코드리뷰 반영
    - 중복코드 getAllAlbumByMemberId로 수정

--- src/main/java/com/pophory/pophoryserver/domain/member/MemberService.java
+++ src/main/java/com/pophory/pophoryserver/domain/member/MemberService.java
@@ -1,20 +1,28 @@
 package com.pophory.pophoryserver.domain.member;
 
 import com.pophory.pophoryserver.domain.album.Album;
+import com.pophory.pophoryserver.domain.album.AlbumJpaRepository;
 import com.pophory.pophoryserver.domain.member.dto.request.MemberCreateRequestDto;
 import com.pophory.pophoryserver.domain.member.dto.response.MemberGetResponseDto;
+import com.pophory.pophoryserver.domain.member.dto.response.MemberMyPageGetResponseDto;
+import com.pophory.pophoryserver.domain.photo.PhotoJpaRepository;
+import com.pophory.pophoryserver.domain.photo.dto.response.PhotoGetResponseDto;
+import com.pophory.pophoryserver.domain.photo.dto.response.PhotoListGetResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityExistsException;
 import javax.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
 
-    public final MemberJpaRepository memberJpaRepository;
+    private final MemberJpaRepository memberJpaRepository;
+    private final AlbumJpaRepository albumJpaRepository;
 
     @Transactional
     public void update(MemberCreateRequestDto request, Long memberId) {
@@ -25,6 +33,13 @@ public class MemberService {
     @Transactional(readOnly = true)
     public MemberGetResponseDto getMember(Long id) {
         return MemberGetResponseDto.of(findMemberById(id));
+    }
+
+    @Transactional(readOnly = true)
+    public MemberMyPageGetResponseDto getMypageMember(Long memberId) {
+        return MemberMyPageGetResponseDto.of(
+                findMemberById(memberId), getPhotoCount(memberId), getPhotos(memberId)
+        );
     }
 
     private void updateMemberInfo(MemberCreateRequestDto request, Long memberId) {
@@ -50,5 +65,26 @@ public class MemberService {
         Album album = new Album();
         album.setCover(albumCover);
         member.getAlbumList().add(album);
+    }
+
+    private int getPhotoCount(Long memberId) {
+        return getAllAlbumByMemberId(memberId).stream().map(
+                album -> album.getPhotoList().size())
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+
+    private PhotoListGetResponseDto getPhotos(Long memberId) {
+        List<PhotoGetResponseDto> photoList = getAllAlbumByMemberId(memberId)
+                .stream()
+                .flatMap(
+                        album -> album.getPhotoList().stream()
+                                .map(PhotoGetResponseDto::of))
+                .collect(Collectors.toList());
+        return PhotoListGetResponseDto.of(photoList);
+    }
+
+    private List<Album> getAllAlbumByMemberId(Long memberId) {
+        return albumJpaRepository.findAllByMemberId(memberId);
     }
 }
