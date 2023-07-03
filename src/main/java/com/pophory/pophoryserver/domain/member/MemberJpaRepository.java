55ca93bcaa0361237979399029ac23957273de29
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-07-03 20:52:13 +0900

    [feat #4] socialType과 socialId로 Member를 검색하는 메소드 추가

--- src/main/java/com/pophory/pophoryserver/domain/member/MemberJpaRepository.java
+++ src/main/java/com/pophory/pophoryserver/domain/member/MemberJpaRepository.java
@@ -1,8 +1,12 @@
 package com.pophory.pophoryserver.domain.member;
 
+import com.pophory.pophoryserver.domain.member.auth.SocialType;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface MemberJpaRepository extends JpaRepository<Member, Long> {
     boolean existsMemberByNickname(String nickname);
-
+    boolean existsMemberBySocialIdAndSocialType(SocialType socialType, String socialId);
+    Optional<Member> getMemberBySocialIdAndSocialType(SocialType socialType, String socialId);
 }
