92a182d47529d78903bb3ea0432f0042cdb19fc4
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-07-03 20:54:37 +0900

    [feat #4] 필요한 요청만 security를 거칠 수 있도록 수정

--- src/main/java/com/pophory/pophoryserver/global/config/SecurityConfig.java
+++ src/main/java/com/pophory/pophoryserver/global/config/SecurityConfig.java
@@ -1,22 +1,53 @@
 package com.pophory.pophoryserver.global.config;
 
+import com.pophory.pophoryserver.global.config.jwt.CustomJwtAuthenticationEntryPoint;
+import com.pophory.pophoryserver.global.config.jwt.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final CustomJwtAuthenticationEntryPoint customJwtAuthenticationEntryPoint;
+
+    private static final String[] AUTH_WHITELIST = {
+            "/api/v1/auth",
+            "/api/v1/test",
+            "/swagger-ui/**",
+            "/swagger-ui.html",
+            "/swagger-ui/index.html",
+            "/docs/swagger-ui/index.html",
+            "/swagger-ui/swagger-ui.css"
+    };
+
     @Bean
+    @Profile("!prod")
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.csrf().disable()
+        return http
+                .csrf().disable()
                 .formLogin().disable()
                 .httpBasic().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)     // 스프링시큐리티가 생성하지도않고 기존것을 사용하지도 않음 -> JWT 방식 사용
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(customJwtAuthenticationEntryPoint)        // 예외처리 기능이 작동
+                .and()
                 .authorizeHttpRequests()
-                .anyRequest().permitAll()
-                .and().build();
+                .antMatchers(AUTH_WHITELIST).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 }
