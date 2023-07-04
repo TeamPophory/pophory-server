c3bafb76ddb04eaa518fb586b826a5839068cd63
unanchoi <unusualc@likelion.org>
2023-07-05 01:18:11 +0900

    [feat #39] 토큰 재발급 API 구현

--- src/main/java/com/pophory/pophoryserver/domain/member/auth/AuthService.java
+++ src/main/java/com/pophory/pophoryserver/domain/member/auth/AuthService.java
@@ -1,18 +1,40 @@
 package com.pophory.pophoryserver.domain.member.auth;
 
+import com.pophory.pophoryserver.domain.member.Member;
 import com.pophory.pophoryserver.domain.member.MemberJpaRepository;
+import com.pophory.pophoryserver.domain.member.auth.dto.TokenVO;
+import com.pophory.pophoryserver.domain.member.auth.dto.response.TokenResponseDto;
+import com.pophory.pophoryserver.global.config.jwt.JwtTokenProvider;
+import com.pophory.pophoryserver.global.config.jwt.UserAuthentication;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
 
     private final MemberJpaRepository memberJpaRepository;
+    private final SocialService socialService;
 
     @Transactional
     public void signOut(Long memberId) {
         memberJpaRepository.deleteById(memberId);
     }
+
+    @Transactional
+    public TokenResponseDto reIssue(Long memberId) {
+        Member member = getMemberById(memberId);
+        TokenVO tokenVO = socialService.generateToken(new UserAuthentication(member.getId(), null, null));
+        member.updateRefreshToken(tokenVO.getRefreshToken());
+        return TokenResponseDto.of(tokenVO.getAccessToken(), tokenVO.getRefreshToken());
+    }
+
+    private Member getMemberById(Long memberId) {
+        return memberJpaRepository.findById(memberId).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다. memberId: " + memberId));
+    }
+
 }
