345201ffd9de0ba4101e50c5b4404183ffe138a9
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-07-17 17:14:55 +0900

    [feat #101] 멤버 아이디 중복체크 API V2 개발

--- src/main/java/com/pophory/pophoryserver/domain/member/controller/MemberV2Controller.java
+++ src/main/java/com/pophory/pophoryserver/domain/member/controller/MemberV2Controller.java
@@ -1,8 +1,10 @@
 package com.pophory.pophoryserver.domain.member.controller;
 
 import com.pophory.pophoryserver.domain.member.MemberService;
+import com.pophory.pophoryserver.domain.member.dto.request.MemberNicknameDuplicateRequestDto;
 import com.pophory.pophoryserver.domain.member.dto.response.MemberGetResponseDto;
 import com.pophory.pophoryserver.domain.member.dto.response.MemberMyPageGetV2ResponseDto;
+import com.pophory.pophoryserver.domain.member.dto.response.MemberNicknameDuplicateResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
@@ -14,10 +16,9 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.security.Principal;
 
 import static com.pophory.pophoryserver.global.util.MemberUtil.getMemberId;
@@ -55,5 +56,18 @@ public class MemberV2Controller {
     })
     public ResponseEntity<MemberGetResponseDto> getMember(Principal principal) {
         return ResponseEntity.ok(memberService.getMember(getMemberId(principal)));
+    }
+
+    @PostMapping
+    @Operation(summary = "멤버 아이디 중복 조회 API")
+    @Parameter(name = "Authorization", description = "Bearer {access_token}", in = ParameterIn.HEADER, required = true, schema = @Schema(type = "string"))
+    @ApiResponses( value = {
+            @ApiResponse(responseCode = "200", description = "아이디 중복 조회 성공"),
+            @ApiResponse(responseCode = "400", description = "사용자 정보 조회 실패", content = @Content),
+            @ApiResponse(responseCode = "500", description = "서버 오류", content = @Content)
+    }
+    )
+    public ResponseEntity<MemberNicknameDuplicateResponseDto> postMemberNickname(@Valid @RequestBody MemberNicknameDuplicateRequestDto request) {
+        return ResponseEntity.ok(memberService.checkDuplicateMemberNickname(request.getNickname()));
     }
 }
