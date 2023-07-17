8385826beb8bf27c9d01078723e1c47027ca22ef
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-07-17 20:54:23 +0900

    [feat #103] responseDto 생성

--- src/main/java/com/pophory/pophoryserver/domain/member/dto/response/MemberCreateResponseDto.java
+++ src/main/java/com/pophory/pophoryserver/domain/member/dto/response/MemberCreateResponseDto.java
@@ -1,2 +1,19 @@
-package com.pophory.pophoryserver.domain.member.dto.response;public class MemberCreateResponseDto {
+package com.pophory.pophoryserver.domain.member.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@AllArgsConstructor(access = PRIVATE)
+@Getter
+@Schema(description = "회원가입")
+public class MemberCreateResponseDto {
+    @Schema(description = "앨범 아이디", example = "1")
+    private int albumId;
+
+    public static MemberCreateResponseDto of(int albumId) {
+        return new MemberCreateResponseDto(albumId);
+    }
 }
