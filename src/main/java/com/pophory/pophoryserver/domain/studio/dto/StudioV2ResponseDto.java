91908410d675dcba010d1a6cd64d9b3dffea5e15
Yunseo Kang <65678579+yungu0010@users.noreply.github.com>
2023-07-19 02:57:20 +0900

    [feat #111] 사진관 목록 전체 조회 API V2 개발 (#117)
    * [feat #111] 사진관 단건 조회 responseDto 생성
    
    * [feat #111] 사진관 전체 조회 responseDto 생성
    
    * [chore #111] 기존 responseDto에 @Scheme 추가
    
    * [feat #111] 사진관 전체 조회 함수 구현
    
    * [feat #111] 사진관 전체 조호 API V2 개발
    
    * [fix #111] id -1일 때 필터링 추가

---
+++ src/main/java/com/pophory/pophoryserver/domain/studio/dto/StudioV2ResponseDto.java
@@ -0,0 +1,22 @@
+package com.pophory.pophoryserver.domain.studio.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Schema(description = "사진관 정보")
+public class StudioV2ResponseDto {
+
+    @Schema(description = "사진관 아이디")
+    private Long id;
+
+    @Schema(description = "사진관 아이디")
+    private String name;
+
+    @Builder
+    public StudioV2ResponseDto(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
