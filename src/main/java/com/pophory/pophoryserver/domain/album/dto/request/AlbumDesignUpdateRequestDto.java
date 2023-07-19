5f082bb1d888c2ecab4d629cf957df513b046b39
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-07-20 02:25:02 +0900

    [fix #138] Schema 수정

--- src/main/java/com/pophory/pophoryserver/domain/album/dto/request/AlbumDesignUpdateRequestDto.java
+++ src/main/java/com/pophory/pophoryserver/domain/album/dto/request/AlbumDesignUpdateRequestDto.java
@@ -10,7 +10,7 @@ import lombok.Getter;
 @Schema(description = "앨범 디자인 수정 DTO")
 public class AlbumDesignUpdateRequestDto {
 
-    @Schema(description = "앨범 디자인", example = "1")
+    @Schema(description = "앨범 디자인 id", example = "1")
     private Long albumDesignId;
 
     @JsonCreator
