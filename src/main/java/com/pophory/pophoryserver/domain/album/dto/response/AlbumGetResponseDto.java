e4a5dcb4573a3e1fa599838801ac06769f388649
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-07-19 19:18:25 +0900

    [hotfix #129] response AlbumCover에서 AlbumDesign으로 수정

--- src/main/java/com/pophory/pophoryserver/domain/album/dto/response/AlbumGetResponseDto.java
+++ src/main/java/com/pophory/pophoryserver/domain/album/dto/response/AlbumGetResponseDto.java
@@ -13,12 +13,12 @@ public class AlbumGetResponseDto {
     private Long id;
     @Schema(description = "앨범 제목", example = "강윤서의 앨범")
     private String title;
-    @Schema(description = "앨범 커버", example = "1")
-    private int albumCover;
+    @Schema(description = "앨범 디자인", example = "1")
+    private Long albumDesignId;
     @Schema(description = "앨범 사진 수", example = "3")
     private int photoCount;
 
     public static AlbumGetResponseDto of(Album album, int count) {
-        return new AlbumGetResponseDto(album.getId(), album.getTitle(), album.getAlbumCover().getCoverNumber(),count);
+        return new AlbumGetResponseDto(album.getId(), album.getTitle(), album.getAlbumDesign().getId(), count);
     }
 }
