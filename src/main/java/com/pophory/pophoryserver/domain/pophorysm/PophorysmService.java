91423203cfc1c14af51e30b523b406bb5203cbd2
Unan <unusualc@likelion.org>
2023-07-21 01:23:23 +0900

    [fix #147] 포포리즘 공유 API 수정 (#148)
    * [feat #128] 포토부스 관련 API 구현
    
    * [fix #128] Photo 공유 수락 API 로직 수정
    
    * [chore #128] swagger 업데이트
    
    * [chore #128] 공백 제거
    
    * [fix #147] 포포리즘 공유 API 수정
    
    * [chore #147] swagger 수정

--- src/main/java/com/pophory/pophoryserver/domain/pophorysm/PophorysmService.java
+++ src/main/java/com/pophory/pophoryserver/domain/pophorysm/PophorysmService.java
@@ -1,5 +1,6 @@
 package com.pophory.pophoryserver.domain.pophorysm;
 
+import com.pophory.pophoryserver.domain.album.Album;
 import com.pophory.pophoryserver.domain.member.Member;
 import com.pophory.pophoryserver.domain.member.MemberJpaRepository;
 import com.pophory.pophoryserver.domain.photo.Photo;
@@ -8,7 +9,10 @@ import com.pophory.pophoryserver.domain.photo.vo.PhotoSizeVO;
 import com.pophory.pophoryserver.domain.pophorysm.dto.request.PophorysmShareRequestDto;
 import com.pophory.pophoryserver.domain.s3.S3Service;
 import com.pophory.pophoryserver.domain.s3.UploadType;
+import com.pophory.pophoryserver.domain.studio.Studio;
+import com.pophory.pophoryserver.domain.studio.StudioJpaRepository;
 import com.pophory.pophoryserver.domain.version.dto.response.PophorysmGetPresignedUrlResponseDto;
+import com.pophory.pophoryserver.global.util.PhotoUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -22,10 +26,14 @@ public class PophorysmService {
 
     private final MemberJpaRepository memberJpaRepository;
     private final PhotoJpaRepository photoJpaRepository;
+    private final StudioJpaRepository studioJpaRepository;
     private final S3Service s3Service;
 
     @Value("${pophorysm.id}")
-    private Long pophorysmId;
+    private Long POPHORYSM_ID;
+
+    @Value("${cloud.aws.CLOUDFRONT}")
+    private String CLOUD_FRONT_DOMAIN;
 
     @Transactional(readOnly = true)
     public PophorysmGetPresignedUrlResponseDto getSharePresignedUrl(UploadType type, String nickname, Long memberId) {
@@ -39,18 +47,28 @@ public class PophorysmService {
     @Transactional
     public void sharePhoto(PophorysmShareRequestDto request, Long memberId) {
         validatePophoryId(memberId);
-        Photo sharedPhoto = getPhotoById(request.getPhotoId());
         Member member = getMemberByNickname(request.getNickname());
-        if (!member.getAlbumList().get(0).checkPhotoLimit()) {
+        Album album = member.getAlbumList().stream().findFirst().orElseThrow(
+                () -> new IllegalArgumentException("앨범이 존재하지 않습니다.")
+        );
+        if (!album.checkPhotoLimit()) {
             throw new IllegalArgumentException("앨범의 사진 개수가 초과되었습니다.");
         }
-        Photo.builder()
-                .album(member.getAlbumList().get(0))
-                .studio(sharedPhoto.getStudio())
-                .takenAt(sharedPhoto.getTakenAt())
-                .imageUrl(sharedPhoto.getImageUrl())
-                .photoSizeVO(PhotoSizeVO.of(request.getWidth(), request.getHeight()))
-                .build();
+        photoJpaRepository.save(
+                 Photo.builder()
+                        .album(member.getAlbumList().get(0))
+                        .studio(findPophorysmStudio(request.getStudioName()))
+                        .takenAt(PhotoUtil.changeRequestToTakenAt(request.getTakenAt()))
+                        .imageUrl(CLOUD_FRONT_DOMAIN + "/" + UploadType.PHOTO.getName() + "member" + member.getPophoryId() + "/" + request.getFileName())
+                        .photoSizeVO(PhotoSizeVO.of(request.getWidth(), request.getHeight()))
+                        .build()
+        );
+    }
+
+    private Studio findPophorysmStudio(String pophorysmStudio) {
+        return studioJpaRepository.findByName(pophorysmStudio).orElseThrow(
+                () -> new IllegalArgumentException("포포리즘 스튜디오가 존재하지 않습니다."));
+
     }
 
     private Member getMemberByNickname(String nickname) {
@@ -71,7 +89,7 @@ public class PophorysmService {
 
 
     private void validatePophoryId(Long memberId) {
-        if (!pophorysmId.equals(memberId)) {
+        if (!POPHORYSM_ID.equals(memberId)) {
             throw new IllegalArgumentException("포포리즘의 사진을 공유할 수 없습니다.");
         }
     }
