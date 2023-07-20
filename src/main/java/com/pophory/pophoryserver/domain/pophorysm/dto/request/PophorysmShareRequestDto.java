c39c6621a59c3121e58be9e3e28f8b5c3ad9c881
unanchoi <unusualc@likelion.org>
2023-07-21 00:38:58 +0900

    [chore #128] swagger 업데이트

--- src/main/java/com/pophory/pophoryserver/domain/pophorysm/dto/request/PophorysmShareRequestDto.java
+++ src/main/java/com/pophory/pophoryserver/domain/pophorysm/dto/request/PophorysmShareRequestDto.java
@@ -8,16 +8,16 @@ import lombok.Data;
 @Data
 public class PophorysmShareRequestDto {
 
-    @Schema(description = "사진 id", example = "")
+    @Schema(description = "사진 id", example = "10")
     private Long photoId;
 
-    @Schema(description = "유저 닉네임", example = "")
+    @Schema(description = "유저 닉네임", example = "kraken")
     private String nickname;
 
-    @Schema(description = "파일 이름", example = "")
+    @Schema(description = "파일 이름", example = "ca4f3c69-a7ac-45ee-a594-d0567a4b66b2.jpg")
     private String fileName;
 
-    @Schema(description = "포포리 id", example = "")
+    @Schema(description = "포포리 id", example = "2d3dfa")
     private String pophoryId;
 
     @Schema(description = "사진 가로 길이", example = "600")
