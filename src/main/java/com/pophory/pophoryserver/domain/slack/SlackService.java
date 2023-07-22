d6d868532e2b5456f3b9e91ab0df3923891d6d07
Unan <unusualc@likelion.org>
2023-07-22 11:37:05 +0900

    [deploy]  회원 가입 시에 slack 알람오는 기능 추가 (#161)
    * [fix #142] 앨범 내 사진 조회 최신순으로 정렬 (#143)
    
    * [fix #140] 앨범 공유 수락 API 로직 수정 (#144)
    
    * [feat #128] 포토부스 관련 API 구현 (#146)
    
    * [feat #128] 포토부스 관련 API 구현
    
    * [fix #128] Photo 공유 수락 API 로직 수정
    
    * [chore #128] swagger 업데이트
    
    * [chore #128] 공백 제거
    
    * [fix #147] 포포리즘 공유 API 수정 (#148)
    
    * [feat #128] 포토부스 관련 API 구현
    
    * [fix #128] Photo 공유 수락 API 로직 수정
    
    * [chore #128] swagger 업데이트
    
    * [chore #128] 공백 제거
    
    * [fix #147] 포포리즘 공유 API 수정
    
    * [chore #147] swagger 수정
    
    * [hotfix #149] 포포리즘 공유 API request body 수정 (#150)
    
    * [hotfix #149] 포포리즘 공유 request body 수정
    
    * [chore #149] 사용하지 않는 import 제거
    
    * [setting #149] banner 추가
    
    * [chore #149] CI 스크립트 수정
    
    * [chore #149] CI prod 스크립트 수정
    
    * [chore #149] cd 스크립트 수정
    
    * [fix #151] 배포 전 버그 수정 (access token 만료 시간 변경, 포포리즘 공유 API 수정) (#152)
    
    * [fix #151] Access Token 만료시간 변경
    
    * [fix #151] 사진 upload url 변경
    
    * [fix #151] 포포리즘 공유시에 이미지 저장 url 변경 (#153)
    
    * [fix #151] Access Token 만료시간 변경
    
    * [fix #151] 사진 upload url 변경
    
    * [hotfix #151] 포포리즘 저장 url 변경
    
    * [refactor #156] slack 회원가입시에 알람 추가 (#157)
    
    * [refactor #156] slack 회원가입시에 알람 추가
    
    * [fix #156] IOException 처리
    
    * [fix #158] slack 메세지 알림 수정 (#159)
    
    * [fix #158] 어노테이션 변경 (#160)
    
    * [fix #158] slack 메세지 알림 수정
    
    * [fix #158] annotation 변경
    
    ---------
    
    Co-authored-by: Yunseo Kang <65678579+yungu0010@users.noreply.github.com>

---
+++ src/main/java/com/pophory/pophoryserver/domain/slack/SlackService.java
@@ -0,0 +1,30 @@
+package com.pophory.pophoryserver.domain.slack;
+
+import com.pophory.pophoryserver.domain.slack.dto.SlackMessageDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+
+@Service
+public class SlackService {
+
+    @Value("${slack.webhook.url}")
+    private String SLACK_WEBHOOK_URL;
+
+    public void sendSignInAlert(String nickname){
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.postForEntity(
+                SLACK_WEBHOOK_URL,
+                createSlackHttpRequest("🎉 " + nickname + "님이 포포리의 회원가입을 완료했습니다. 🎉"),
+                String.class);
+    }
+
+    private HttpEntity<SlackMessageDto> createSlackHttpRequest(String text) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Accept", "application/json; UTF-8");
+        return new HttpEntity<>(SlackMessageDto.of(text), headers);
+    }
+}
