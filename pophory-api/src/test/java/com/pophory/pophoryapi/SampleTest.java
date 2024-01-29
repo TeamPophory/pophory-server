3fbec7d7ab6914b798f43eb21ccbe5540fff4f44
unanchoi <unanc2@gmail.com>
2024-01-30 00:18:52 +0900

    [test] sample test 추가

---
+++ pophory-api/src/test/java/com/pophory/pophoryapi/SampleTest.java
@@ -0,0 +1,18 @@
+package com.pophory.pophoryapi;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SampleTest {
+
+    @Test
+    @DisplayName("샘플 테스트")
+    void test() {
+      // given
+
+      // when
+
+      // then
+
+    }
+}
